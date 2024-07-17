68e01acf63ae2550a87b6edfb7d8270006d03284
Adrian Flak <kontakt@adrianflak.pl>
2024-07-17 12:18:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// console.log("Hello World");
+// anime({
+//     targets: '.img-hero',
+//     scale: {
+//         value: 2,
+//         duration: 1600,
+//         delay: 800,
+//         easing: 'easeInOutQuart'
+//       },
+//       delay: 250 
+//   });
